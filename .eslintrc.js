e05498fb53ab5144ff77b80fda5b139314abc2a3
Joshua Reeson <joshua.reeson@dukecannon.com>
2026-05-20 18:17:43 -0500

    Add ESLint config for n8n-nodes-base plugin

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['./tsconfig.json'],
+		sourceType: 'module',
+		extraFileExtensions: ['.json'],
+	},
+	extends: ['plugin:n8n-nodes-base/nodes'],
+	rules: {},
+};
